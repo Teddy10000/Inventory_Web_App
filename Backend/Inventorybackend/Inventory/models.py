86304820e938b753b9c80627986ccb222819b907
Teddy10000 <tedakrong12@gmail.com>
2023-12-21 22:12:16 +0000

    Setting up the inventory models to take care of the inventory

--- Backend/Inventorybackend/Inventory/models.py
+++ Backend/Inventorybackend/Inventory/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Ingredients(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=40)
+    quantity = models.IntegerField()
+    unit_of_measurement = models.CharField(max_length=40)
+
